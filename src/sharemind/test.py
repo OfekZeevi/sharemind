f90f5c3bf6a5f9dbecc206e0a13eb7d5431f9e72
Ofek Zeevi <zeevi.ofek@gmail.com>
2025-01-19 15:59:32 +0200

    Improve test script
    Renamed to make its purpose clearer. Also - lower randomization upper bound to fix a rare bug that could cause an invalid example to run

--- src/sharemind/test.py
+++ src/sharemind/test.py
@@ -7,7 +7,7 @@ def main():
     for n in [8, 16, 32, 64]:
         count = 0
         for _ in tqdm(range(1000)):
-            i, j = [random.randint(0, 2**(n-1)) for _ in range(2)]
+            i, j = [random.randint(0, 2**(n-1)-1) for _ in range(2)]
             a = SharemindSecret(value=i, size=n)
             b = SharemindSecret(value=j, size=n)
             if (i * j) % (2 ** n) != (a * b).numeric_value or bool(i >= j) ^ bool(a >= b):
